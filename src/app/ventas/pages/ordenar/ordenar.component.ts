b094feb7c939d9cb256736d23cdf47c647ae8f27
blilllly <alvearbilly@gmail.com>
2022-07-28 13:47:02 -0500

    end

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,11 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
-  constructor() { }
+  enMayusculas: boolean = true;
+  ordenarPor: string = '';
 
-  ngOnInit(): void {
+  heroes: Heroe[]=[
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: 'Daredevil',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    }
+  ]
+
+  heroes2: Heroe[]= [...this.heroes];
+
+  cambiarPropiedad () {
+    this.enMayusculas = !this.enMayusculas;
+  }
+
+  cambiarOrden (value: string) {
+    this.ordenarPor = value;
   }
 
 }
